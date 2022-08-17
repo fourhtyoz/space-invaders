11411418d28ba165c51f72683be7f6ef3191bb26
meow <abc@gmail.com>
2022-08-17 14:44:24 +0300

    movements and space ship

--- main.py
+++ main.py
@@ -3,8 +3,11 @@ import os
 import time
 import random
 
+# Init the font to write on the screen
+pygame.font.init()
+
 # Screen
-WIDTH, HEIGHT = 650, 650
+WIDTH, HEIGHT = 800, 600
 WIN = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption('Space Invaders')
 
@@ -25,19 +28,61 @@ YELLOW_LASER = pygame.image.load(os.path.join('assets', 'pixel_laser_yellow.png'
 # Background
 BG = pygame.transform.scale(pygame.image.load(os.path.join('assets', 'background-black.png')), (WIDTH, HEIGHT))
 
+# Abstract/Base class
+class Ship:
+    def __init__(self, x, y, health=100):
+        self.x = x
+        self.y = y
+        self.health = health
+        self.ship_img = None # Allow to draw ship and laser but the class is abstract, so I put None
+        self.laser_img = None
+        self.lasers = []
+        self.cool_down_counter = 0
+    
+    def draw(self, window):
+        window.blit(self.ship_img, (self.x, self.y))
+
+    def get_width(self):
+        return self.ship_img.get_width()
+    
+    def get_height(self):
+        return self.ship_img.get_height()
+
+
+class Player(Ship):
+    def __init__(self, x, y, health=100):
+        super().__init__(x, y, health)
+        self.ship_img = YELLOW_SPACE_SHIP
+        self.laser_img = YELLOW_LASER
+        self.mask = pygame.mask.from_surface(self.ship_img) # creating a pixel=perfect hitbox
+        self.max_health = health
+
+
 # Main loop
 def main():
     run = True
     FPS = 60
-    level = 10
+    level = 1
     lives = 5
-    
+    player_vel = 10
+    main_font = pygame.font.SysFont('comicsams', 50)
+
+    player = Player(100, 100)
+
     clock = pygame.time.Clock()
 
     # Updating the window
     def redraw_window():
         # First - blit the BG
         WIN.blit(BG, (0, 0))
+        # Draw labels
+        level_label = main_font.render(f"Level: {level}", 1, (255, 255, 255))
+        lives_label = main_font.render(f"Lives: {lives}", 1, (255, 255, 255))
+        WIN.blit(level_label, (10, 10))
+        WIN.blit(lives_label, (WIDTH - level_label.get_width() - 10, 10))
+        
+        player.draw(WIN)
+
         pygame.display.update()
 
     while run:
@@ -49,5 +94,15 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
+
+        keys = pygame.key.get_pressed() # return a dict of keys when they're pressed.
+        if keys[pygame.K_a] and player.x + player_vel > 0: # left
+            player.x -= player_vel
+        if keys[pygame.K_d] and player.x + player_vel + player.get_width() < WIDTH: # right
+            player.x += player_vel
+        if keys[pygame.K_w] and player.y - player_vel > 0: # up
+            player.y -= player_vel
+        if keys[pygame.K_s] and player.y + player_vel + player.get_height() < HEIGHT: # down
+            player.y += player_vel
 main()
                 
